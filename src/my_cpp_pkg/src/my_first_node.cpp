3857d1874d88e14a50346d2651201237578b4f99
vboxuser <vboxuser@ros2.myguest.virtualbox.org>
2024-02-18 19:08:50 +0530

    write cpp node with oops

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,12 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+public:
+    MyNode() : Node("cpp_test"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "hello cpp from class Node");
+        timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyNode::timerCallback, this));
+    }
+
+private:
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "hello  %d",counter_);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+};
 
 int main(int argc, char **argv)
 {
     /* code */
-    rclcpp::init(argc,argv);
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "hello cpp Node");
+    rclcpp::init(argc, argv);
+    // auto node = std::make_shared<rclcpp::Node>("cpp_test");
+    // RCLCPP_INFO(node->get_logger(), "hello cpp Node");
+    auto node = std::make_shared<MyNode>();
+
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
