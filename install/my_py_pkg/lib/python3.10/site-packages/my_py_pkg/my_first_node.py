38b2f85021a49d3823aa989865ef98568e790470
vboxuser <vboxuser@ros2.myguest.virtualbox.org>
2024-02-18 17:34:10 +0530

    write a python Node With OOP

--- install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/my_first_node.py
+++ install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/my_first_node.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.counter_=0
+        self.get_logger().info("hello ros2 from class")
+        self.create_timer(0.5,self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ +=1
+        self.get_logger().info("hello "+str(self.counter_))
+    
+
 def main(args=None):
     rclpy.init(args=args)
-    node=Node("py_test")
-    node.get_logger().info("hello ros2")
+    node=MyNode()
+    # node=Node("py_test")
+    # node.get_logger().info("hello ros2")
     rclpy.spin(node)
     rclpy.shutdown();
 
