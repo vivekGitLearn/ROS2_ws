f053ad96a7b6159c007401d6e088d1400c51e249
vboxuser <vboxuser@ros2.myguest.virtualbox.org>
2024-02-18 17:18:02 +0530

    write a python minimal code

---
+++ install/my_py_pkg/lib/python3.10/site-packages/my_py_pkg/my_first_node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+def main(args=None):
+    rclpy.init(args=args)
+    node=Node("py_test")
+    node.get_logger().info("hello ros2")
+    rclpy.spin(node)
+    rclpy.shutdown();
+
+if __name__=="__main__":
+    main()
