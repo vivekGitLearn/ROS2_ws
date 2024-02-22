edc2b6821b6458db554aef963dfcc0e71e1d643a
vboxuser <vboxuser@ros2.myguest.virtualbox.org>
2024-02-23 00:54:46 +0530

    write python add_two_number_server_node

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -24,7 +24,8 @@ setup(
             "robot_news_station = my_py_pkg.robot_news_station:main",
             "smartphone = my_py_pkg.smartphone:main",
             "number_publisher = my_py_pkg.number_publisher:main",
-            "number_counter = my_py_pkg.number_counter:main"
+            "number_counter = my_py_pkg.number_counter:main",
+            "add_two_ints_server = my_py_pkg.add_two_ints_server:main"
         ],
     },
 )
